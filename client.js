c1c246ac544a61ee6bb0bc5196936201851bb214
Jessica <Jessica.Hilario@student.csulb.edu>
2019-04-21 22:35:40 -0700

    Create the paths to add a user

--- client.js
+++ client.js
@@ -1,8 +1,33 @@
+const http = require('http')
 const electron = require('electron')
 
+const ip = '104.237.158.50'
 const port = 8080
 
 function validate(email, password)
 {
-	get = 
+}
+
+function register(info)
+{
+	payload = JSON.stringify(info)
+
+	options = {
+		host: ip,
+		port: port,
+		method: 'POST',
+		path: '/registerUser',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': Buffer.byteLength(info)
+		}
+	}
+
+	const req = http.request(options,
+		(res) => {
+			console.log("Sent")
+		})
+
+	req.write(info)
+	req.end()
 }
