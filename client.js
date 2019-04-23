72fcb26e83897936f981e25d30f56350297b6a42
Jessica <Jessica.Hilario@student.csulb.edu>
2019-04-22 21:37:34 -0700

    Included the yelp api and modified the signUpPage.html form to send the data to the client

--- client.js
+++ client.js
@@ -1,17 +1,18 @@
 const http = require('http')
 const electron = require('electron')
 
+//Initialize ip address and port
 const ip = '104.237.158.50'
 const port = 8080
 
-function validate(email, password)
+//Registers the user into the database
+function register()
 {
-}
-
-function register(info)
-{
+	console.log("here")
+	//Turns the info into a JSON string
 	payload = JSON.stringify(info)
-
+	
+	//Where we want it to be sent and in what form
 	options = {
 		host: ip,
 		port: port,
@@ -19,15 +20,16 @@ function register(info)
 		path: '/registerUser',
 		headers: {
 			'Content-Type': 'application/json',
-			'Content-Length': Buffer.byteLength(info)
+			'Content-Length': Buffer.byteLength(payload)
 		}
 	}
 
+	//Send the request through http by what we said in the options
 	const req = http.request(options,
 		(res) => {
 			console.log("Sent")
 		})
 
-	req.write(info)
+	req.write(payload)
 	req.end()
 }
