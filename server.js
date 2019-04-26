4761320f29d25d00ef54f0854b2fcab9f74d89a4
BUNBUN\Deathrabbit <hilario.jessica97@gmail.com>
2019-04-26 09:09:46 -0700

    It's converting time

--- server.js
+++ server.js
@@ -1,31 +1,54 @@
 const express = require('express')
 const app = express()
 const crypto = require('crypto')
-const mongoose = require('mongoose')
-const User = require('./db/user')
-
-var  bodyParser = require("body-parser")
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-
-mongoose.Promise = global.Promise
-mongoose.connect("mongodb://localhost:27017/letseat", { useNewURLParser: true })
-
-const ip = '104.237.158.50'
+const os = require('os') //What is this for?
+const ip = require('ip')
+const events = require('events')
+const electron = require('electron')
+const {ipcRenderer} = electron
+const hostIP = ip.address()
 const port = 8080
+
+//var  bodyParser = require("body-parser")
+//app.use(bodyParser.json())
+//app.use(bodyParser.urlencoded({ extended: true }))
+
 var username
+var users = []
 var user = {}
 var pref = {}
 
 app.post("/registerUser", (req,res) => {
-    console.log(req.body)
+    var body = []
     
-//   var key = Object.keys(req.body)
-//   console.log(typeof req.body)
-//   var info = JSON.parse(key)
-//   console.log(info)
+    req.on("data", (chunk) => {
+        body.push(chunk)
+        console.log(body)
+    }).on("end", () => {
+        var info = JSON.parse(Buffer.concat(body).toString())
+        console.log(info)
 
-   let info = req.body
+       username = info.Username
+       var pw = info.Password
+      
+      //Create a salt and a salted hash
+       const salt = crypto.randomBytes(16)
+       const shash = crypto.createHash('sha256').update(salt + pw).digest()
+       var user = {
+           userName: username,
+           salt: salt,
+           shash: shash,
+           firstName: info.FirstName,
+           lastName: info.LastName,
+           email: info.Email,
+           city: info.City,
+           state: info.State,
+           sQuestion: info.SecurityQuestion,
+           sAnswer: info.Answer
+       }
+    })
+
+   /*let info = req.body
 
    username = info.Username
    var pw = info.Password
@@ -60,7 +83,7 @@ app.post("/registerUser", (req,res) => {
             res.writeHead(469, "Username/email taken")
             res.end()
         })
-
+*/
 })
 
 app.listen(port, ip, () => {
