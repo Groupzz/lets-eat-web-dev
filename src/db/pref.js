9d8c145bcd10bcd5d1614e1d08d1912932456d5f
BUNBUN\Deathrabbit <hilario.jessica97@gmail.com>
2019-06-02 21:37:27 -0700

    Moved all updated and non-updated files to the folder src

--- src/db/pref.js
+++ src/db/pref.js
@@ -3,7 +3,7 @@ var Schema = mongoos.Schema;
 
 var prefSchema = new Schema({
     _id: mongoose
-    preference: String
-});
+        preference: String
+        });
 
-var pref = mongoose.model('Preference', prefSchema);
+        var pref = mongoose.model('Preference', prefSchema);
--- src/db/user.js
+++ src/db/user.js
@@ -4,18 +4,18 @@ var Schema = mongoose.Schema
 //Creating the table
 var usersSchema = new Schema({
     userName: String,
-    salt: Buffer,
-    shash: Buffer,
-    firstName: String,
-    lastName: String,
-    email: String,
-    city: String,
-    state: String,
-    sQuestion: String,
-    sAnswer: String
-})
+        salt: Buffer,
+            shash: Buffer,
+                firstName: String,
+                    lastName: String,
+                        email: String,
+                            city: String,
+                                state: String,
+                                    sQuestion: String,
+                                        sAnswer: String
+                                        })
 
-//To use the schema definition (Table)
-var User = mongoose.model('User', usersSchema)
+                                        //To use the schema definition (Table)
+                                        var User = mongoose.model('User', usersSchema)
 
-module.exports = User
+                                        module.exports = User
