a93cad009b1d076435c3f3a0b4fca1b782311aa3
Jessica Hilario <jessica.hilario@student.csulb.edu>
2019-09-03 11:16:27 -0700

    added firebase

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase/app");
+
+require("firebase/firestore");
+require("firebase/auth");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCvKLDrNaPfCcEIlvddM4MWXFSbTs4SmT0",
+  authDomain: "lets-eat-18b7b.firebaseapp.com",
+  databaseURL: "https://lets-eat-18b7b.firebaseio.com",
+  projectId: "lets-eat-18b7b",
+  storageBucket: "lets-eat-18b7b.appspot.com",
+  messagingSenderId: "852218951035",
+  appId: "1:852218951035:web:f66d1e057e50f8d4"
+};
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
+  db.collection('friends').get().then((snapshot) => {
+    snapshot.docs.forEach(doc => {
+      console.log(doc.data());
+    })
+  });
   res.render('index', { title: 'Express' });
 });
 
