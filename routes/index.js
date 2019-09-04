50ced6a5fa23714d6bdfd52264c71c6ee01985ee
BUNBUN\Deathrabbit <hilario.jessica97@gmail.com>
2019-09-04 11:54:54 -0700

    Made the css

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var firebase = require("firebase/app");
+var nodemailer = require("nodemailer");
+var bcrypt = require('bcrypt-nodejs');
 
 require("firebase/firestore");
 require("firebase/auth");
@@ -17,6 +19,23 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+// users - city, dateofbirth, email, firstname, lastname, password, phone, securityanswer, securityquestion, state, username, zipcode
+// preferences - username, preference
+// statusboard - checkin, username
+
+// Sending emails to the user
+let smtpTransport = nodemailer.createTransport({
+  host: "smtp-mail.outlook.com", // hostname
+  secureConnection: false, // TLS requires secureConnection to be false
+  port: 587, // port for secure SMTP
+  tls: {
+    ciphers:'SSLv3'
+  },
+  auth: {
+    user: 'register@pvnet.com',
+    pass: 'R3gi$t3r'
+  }
+});
 
 /* GET index page. */
 router.get('/', function(req, res, next) {
