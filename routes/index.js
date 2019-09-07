f9c887968fe8a9a2a34725074243f7e09e773993
BUNBUN\Deathrabbit <hilario.jessica97@gmail.com>
2019-09-07 11:38:27 -0700

    Revert "I forgot what I did."
    This reverts commit 1098dccfbc7ced1d9c0852562985aa1b10d2e019.

--- routes/index.js
+++ routes/index.js
@@ -21,22 +21,21 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
-// users - city, dateofbirth, email, firstname, lastname, password, phone, securityanswer, securityquestion, state, username, zipcode(num)
+// users - city, dateofbirth, email, firstname, lastname, password, phone, securityanswer, securityquestion, state, username, zipcode
 // preferences - username, preference
 // statusboard - checkin, username
-// DietaryRestriction - username, Vegan, Vegetarian, Halal,
 
 // Sending emails to the user
 let smtpTransport = nodemailer.createTransport({
-  host: "imap.gmail.com", // hostname
+  host: "smtp-mail.outlook.com", // hostname
   secureConnection: false, // TLS requires secureConnection to be false
   port: 587, // port for secure SMTP
   tls: {
     ciphers:'SSLv3'
   },
   auth: {
-    user: 'letseatsc@gmail.com',
-    pass: 'Aaqwertyuiop1'
+    user: 'register@pvnet.com',
+    pass: 'R3gi$t3r'
   }
 });
 
