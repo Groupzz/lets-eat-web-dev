b413158c6aac6bf56cfc736b8ebe79f6c11f57d7
BUNBUN\Deathrabbit <hilario.jessica97@gmail.com>
2019-09-11 18:17:07 -0700

    Implemented firebase

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var firebase = require("firebase/app");
+require("firebase/firestore");
 // var nodemailer = require("nodemailer");
 // var bcrypt = require('bcrypt-nodejs');
 // var yelp = require('yelp-fusion');
-var admin = require("firebase-admin");
-var functions = require('firebase-functions');
 
-var serviceAccount = require(".\\serviceAccountKey.json");
-
-admin.initializeApp(
-    // credential: firebase.credential.cert(serviceAccount),
-    // databaseURL: "https://lets-eat-18b7b.firebaseio.com"
-    functions.config().firebase
-);
-var db = admin.firestore();
-// users - city, dateofbirth, email, firstname, lastname, password, phone, securityanswer, securityquestion, state, username, zipcode
-// preferences - username, preference
-// statusboard - checkin, username
+// setup connection to firebase database
+const firebaseConfig = {
+    apiKey: "AIzaSyCvKLDrNaPfCcEIlvddM4MWXFSbTs4SmT0",
+    authDomain: "lets-eat-18b7b.firebaseapp.com",
+    databaseURL: "https://lets-eat-18b7b.firebaseio.com",
+    projectId: "lets-eat-18b7b",
+    storageBucket: "lets-eat-18b7b.appspot.com",
+    messagingSenderId: "852218951035",
+    appId: "1:852218951035:web:f66d1e057e50f8d4"
+};
+firebase.initializeApp(firebaseConfig);
+// Create the database connection
+const db = firebase.firestore();
 
 // Sending emails to the user
 // let smtpTransport = nodemailer.createTransport({
@@ -31,51 +33,32 @@ var db = admin.firestore();
 //     pass: 'R3gi$t3r'
 //   }
 // });
-// var firebase = require("firebase/app");
-// //
-// // Add the Firebase products that you want to use
-// require("firebase/auth");
-// require("firebase/firestore");
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCvKLDrNaPfCcEIlvddM4MWXFSbTs4SmT0",
-//     authDomain: "lets-eat-18b7b.firebaseapp.com",
-//     databaseURL: "https://lets-eat-18b7b.firebaseio.com",
-//     projectId: "lets-eat-18b7b",
-//     storageBucket: "lets-eat-18b7b.appspot.com",
-//     messagingSenderId: "852218951035",
-//     appId: "1:852218951035:web:f66d1e057e50f8d4"
-// };
-// firebase.initializeApp(firebaseConfig);
-// const db = firebase.firestore();
-function getUsername() {
-    var docRef = db.collection("testing").document("name");
-
-    docRef.get()
-        .then(function(doc) {
-        if (doc.exists) {
-            console.log("Document data:", doc.data());
-            var name = doc.get('name');
-            console.log(name);
-        } else {
-            console.log("No such document!");
-        }
-        }) .catch(function(error) {
-            console.log("Error getting document:", error);
-        });
-    // const ref = db.ref('friends');
-    // ref.once('value')
-    //     .then(snap => {
-    //     console.log(snap.val());
-    //     console.log(snap.val().username);
-    //     })
-    //     .catch(err => {
-    //         console.log(err);
-    //     });
-}
 /* GET index page. */
 router.get('/', function(req, res, next) {
-    getUsername();
-  res.render('index', { title: 'Express'});
+    var cafes = [];
+    // Create a promise for the cafes
+    const cafePromise = new Promise((res, rej) => {
+        // called the table database
+        db.collection('cafes').get()
+            .then((snapshot) => {
+                // loop through each document in the database
+                snapshot.docs.forEach(doc => {
+                    // grab the data and push it to a list
+                    var details = {
+                        id: doc.id,
+                        name: doc.data().name,
+                        city: doc.data().city
+                    };
+                    cafes.push(details);
+                });
+                // resolve the details of cafes
+                res(cafes);
+            });
+    })
+        .then(cafeList => {
+            var buffer = "cafes";
+            res.render('index', { title: 'Express' , data: buffer, cafeList});
+        });
 });
 
 /* GET home page. */
